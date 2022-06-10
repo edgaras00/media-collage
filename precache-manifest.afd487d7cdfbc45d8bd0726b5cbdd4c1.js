f1ba2745eb1295219bcf6652001a0b62dabf6e54
Edgar <edgaras993@gmail.com>
2022-06-09 21:20:23 -0400

    Updates

---
+++ precache-manifest.afd487d7cdfbc45d8bd0726b5cbdd4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920857e9ccb9507b5f57f7c4728234fd",
+    "url": "/media-collage/index.html"
+  },
+  {
+    "revision": "c41926d16b3950f83816",
+    "url": "/media-collage/static/css/main.fc32d9e0.chunk.css"
+  },
+  {
+    "revision": "47d30eddcfbc9d74aad1",
+    "url": "/media-collage/static/js/2.c0c6ab7d.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/media-collage/static/js/2.c0c6ab7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41926d16b3950f83816",
+    "url": "/media-collage/static/js/main.f1a8da85.chunk.js"
+  },
+  {
+    "revision": "045dfa14be07a52a214f",
+    "url": "/media-collage/static/js/runtime-main.69fe505c.js"
+  }
+]);
